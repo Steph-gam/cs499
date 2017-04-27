14af69d6c20e05429b78e7b8c0aa6ef75bf53f94
Steph-Gam <gamboastephanie@live.com>
2017-04-26 17:26:27 -0700

    first commit

--- app/src/main/java/com/example/stephanie/cs499/MainActivity.java
+++ app/src/main/java/com/example/stephanie/cs499/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.stephanie.cs499;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button tutorialsButton = (Button) findViewById(R.id.tutorials);
+        tutorialsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TutorialsActivity.class));
+            }
+        });
+
+        Button tipsButton = (Button) findViewById(R.id.tips);
+        tipsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, TipsActivity.class));
+            }
+        });
+
+        Button popularButton = (Button) findViewById(R.id.popular);
+        popularButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, PopularActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/stephanie/cs499/TutorialsActivity.java
@@ -0,0 +1,20 @@
+package com.example.stephanie.cs499;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+/**
+ * Created by Stephanie on 4/26/17.
+ */
+public class TutorialsActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorials);
+
+    }
+}
